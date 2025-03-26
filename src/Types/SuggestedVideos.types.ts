f65ad918db181d534205b2dfbb1159e3ccde813e
Omar Alaa Samir Nawar <omar.nawar@flairstech.com>
2025-03-26 21:27:06 +0200

    Finished responsiveness for all devices, completed SuggestedVideos component, finalized Videos page layout, and completed website 1

--- src/Types/SuggestedVideos.types.ts
+++ src/Types/SuggestedVideos.types.ts
@@ -1,12 +1,12 @@
 export interface Video {
-    avatar: string;
-    videoUrl: string;
-    channel: string;
-    title: string;
-    views: string;
-  }
-  
-  export interface SuggestedVideosProps {
-    videos: Video[];
-  }
-  
+  avatar: string;
+  videoUrl: string;
+  channel: string;
+  title: string;
+  views: string;
+}
+
+export interface SuggestedVideosProps {
+  videos?: Video[];
+  onVideoSelect: (video: Video) => void; // ✅ Function to update main video
+}
